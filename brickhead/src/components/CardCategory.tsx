510a55c3421478dd3c862673829f0cebe4be2db6
yetiblue <thempver@gmail.com>
2022-01-25 15:44:43 -0500

    first push

---
+++ brickhead/src/components/CardCategory.tsx
@@ -0,0 +1,13 @@
+import "./CardCategory.css"
+function CardCategory(props:any){
+
+    return(
+        <div className="category-wrapper">
+            <p className="category-type">Category</p>
+             {/* <p>{props.title}</p> */}
+
+        </div>
+    )
+}
+
+export default CardCategory
