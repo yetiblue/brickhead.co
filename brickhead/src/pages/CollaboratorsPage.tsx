e250178e434c5f56014d4d8565f45951f2cd5d80
yetiblue <thempver@gmail.com>
2022-02-05 21:39:44 -0500

    changed collab name font size

--- brickhead/src/pages/CollaboratorsPage.tsx
+++ brickhead/src/pages/CollaboratorsPage.tsx
@@ -1,7 +1,11 @@
 import HeaderComponent from "../components/HeaderComponent";
 import CollaboratorList from "../components/CollaboratorList";
+import { useEffect } from "react";
 
 function HomePage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <HeaderComponent />
