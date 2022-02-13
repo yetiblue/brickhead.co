1b4730d899b81f7ca99d24c1d30aae64990e9171
yetiblue <thempver@gmail.com>
2022-02-12 22:45:03 -0500

    home page progress desktop

--- brickhead/src/components/MainVideo.tsx
+++ brickhead/src/components/MainVideo.tsx
@@ -3,7 +3,7 @@ import "./MainVideo.css";
 function MainVideo() {
   return (
     <div>
-      <video className="reel-main" controls autoPlay muted loop>
+      <video className="reel-main" muted loop playsInline>
         <source
           src="http://dzkagq791g2eh.cloudfront.net/Brickhead-stills/3092950152.mp4"
           type="video/mp4"
--- brickhead/src/pages/HomePage.tsx
+++ brickhead/src/pages/HomePage.tsx
@@ -2,14 +2,16 @@ import HeaderComponent from "../components/HeaderComponent";
 import HomeGrid from "../components/HomeGrid";
 import MainVideo from "../components/MainVideo";
 import { useEffect } from "react";
-
+import "./HomePage.css";
 function HomePage() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <div>
-      <HeaderComponent />
+      <div className="header">
+        <HeaderComponent />
+      </div>
       <MainVideo />
       <HomeGrid />
     </div>
